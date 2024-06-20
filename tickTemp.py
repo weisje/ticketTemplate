80896512473259b385442989c9a5969693c724cc
weisje <20269574+weisje@users.noreply.github.com>
2024-06-20 08:25:38 -0400

    added function to check for current time of day & return appropriate greeting.  Added check for template to provide basic issue template.  Otherwise it'll deliver the specified message to the caller.

--- tickTemp.py
+++ tickTemp.py
@@ -1,5 +1,24 @@
+import pyperclip
 import sys
+import datetime
 
+
+def timeOfDay() -> str:
+    """
+    Function to get the current hour & provide a "Greeting" to the caller based on the hour of the day
+    :return: str
+    """
+    currentHour = datetime.datetime.now().hour
+    currentTimeOfDay = "Day"
+
+    if currentHour < 12 and currentHour >= 3:
+        currentTimeOfDay = "Morning"
+    elif currentHour < 17 and currentHour >= 12:
+        currentTimeOfDay = "Afternoon"
+    elif currentHour < 3 and currentHour >= 17:
+        currentTimeOfDay = "Evening"
+
+    return currentTimeOfDay
 
 def main(keyphrase) -> None:
     """
@@ -8,7 +27,24 @@ def main(keyphrase) -> None:
     :type keyphrase: str
     :return: None
     """
-    pass
+    TEXT = {"agree": """This is true.  I agree.""",
+            "busy": """I apologize but I have a lot going on right now.  Can this wait?""",
+            "template": """Username:
+Customer: N/A
+Description:
+Error: N/A
+Verdict:
+Article:"""}
+    timeGreeting = timeOfDay()
+
+    if keyphrase in TEXT:
+        if keyphrase == "template":
+            print(TEXT[keyphrase])
+        else:
+            print(f"\nGood {timeGreeting},\n\n{TEXT[keyphrase]}\n")
+    else:
+        print(f"\'{keyphrase}\' is not a valid selection.")
+        sys.exit()
 
 
 if __name__ == '__main__':
