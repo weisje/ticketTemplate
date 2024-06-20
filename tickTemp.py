70b50908f99ac6ae5b580ed8ecca2a4e0a8f3d25
weisje <20269574+weisje@users.noreply.github.com>
2024-06-20 08:56:13 -0400

    Added more example text to TEXT dictionary

--- tickTemp.py
+++ tickTemp.py
@@ -1,6 +1,7 @@
-import pyperclip
-import sys
 import datetime
+import pyperclip
+import random
+import sys
 
 
 def timeOfDay() -> str:
@@ -20,6 +21,24 @@ def timeOfDay() -> str:
 
     return currentTimeOfDay
 
+
+def cannedGreeting(greetingType="general") -> str:
+    """
+    Function to take in selection from caller and provide a randomly selected "greeting" for the message being returned
+    :param greetingType: type of greeting that should be returned to the caller. if the greeting requested is not found, a boilerplate general message will be returned.
+    :type greetingType: str
+    :return: str
+    """
+    greetings = {"request": ["Thank you for submitting this request",
+                             "Thank you for submitting this service request"],
+                 "general": ["Thank you for reaching out about this",
+                             "Thank you for bringing this to our attention"]}
+
+    if greetingType in greetings:
+        return random.choice(greetings[greetingType])
+    else:
+        return random.choice(greetings["general"])
+
 def main(keyphrase) -> None:
     """
     Main function for tickTemp.py to copy user's keyphrase to the clipboard
@@ -27,8 +46,9 @@ def main(keyphrase) -> None:
     :type keyphrase: str
     :return: None
     """
-    TEXT = {"agree": """This is true.  I agree.""",
-            "busy": """I apologize but I have a lot going on right now.  Can this wait?""",
+    TEXT = {"accountworksnoaction": "We have reviewed your account and you should be able to access it now.  If you still cannot access it in 48 hours let us know & we will troubleshoot further.",
+            "accountworksaction": "We have made adjustments to your account and you should be able to access it now.  If you still cannot access it in 48 hours let us know & we will troubleshoot further.",
+            "sendtohr": "After reviewing this we have determined that it will need to be reviewed by the Human Resources department.  They can be reached at hr@iqtech.555 or by phone at 555-555-5555. They will be able to assist you further.",
             "template": """Username:
 Customer: N/A
 Description:
@@ -36,12 +56,24 @@ Error: N/A
 Verdict:
 Article:"""}
     timeGreeting = timeOfDay()
+    introGreetng = cannedGreeting()
+    cannedSalutations = ["Have a good day",
+                         "Take care",
+                         "Have a great day"]
+
+    salutation = random.choice(cannedSalutations)
+
+    signature = """John Q. Smith
+Senior Help Desk Technician
+Information Technology Department
+IQ Technology
+"""
 
     if keyphrase in TEXT:
         if keyphrase == "template":
             print(TEXT[keyphrase])
         else:
-            print(f"\nGood {timeGreeting},\n\n{TEXT[keyphrase]}\n")
+            print(f"\nGood {timeGreeting},\n\n{introGreetng}. {TEXT[keyphrase]} {salutation}.\n\n{signature}")
     else:
         print(f"\'{keyphrase}\' is not a valid selection.")
         sys.exit()
